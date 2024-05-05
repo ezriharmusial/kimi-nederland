cc6620ee15347e30e80b04aa7c19426d42bf3613
ezriharmusial <ezriharmusial@gmail.com>
2024-05-05 15:04:08 +0100

    site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://ovidius-astro-theme.netlify.app',
+    site: 'https://kimi-nederland.netlify.app',
     integrations: [
         mdx(),
         sitemap(),
